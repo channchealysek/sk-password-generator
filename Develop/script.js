abdf8f91b6001d61572688eb458d842e28befaa7
channchealy <sek.channchealy@yahoo.com>
2022-06-10 21:58:11 -0500

    add action to function generatePassword()

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,63 @@
 // Assignment code here
+var number = "0123456789";
+var symbol = "!@#$%&*_-+=|()[]:“‘>,.?/";
+var upper_cases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower_cases = "abcdefghIjklmnpoqrstuvwxyz";
+var user_char;
+var rand;
+
+//function password Generator
+function generatePassword() {
+
+  var digit= prompt("Choose a length of at least 8 characters and no more than 128 characters for your password.\nif you don't choose any lengths for password.\nSystem will set default lenghts to 12 characters.");
+  if(digit==="") digit = 12;
+  console.log(digit);
+  var user_char = prompt("Enter character that you want to include in the password.\adon't enter any characters.\nSystem will generate by DEFAULT.");
+  console.log(user_char);
+
+  let password = "";
+
+  for( let i=0; i<digit; i++) {
+    
+    if (user_char === ""){
+      rand = Math.floor(Math.random()*4);
+    }else{
+      rand = Math.floor(Math.random()*5);
+
+    } 
+    switch(rand) {
+      case 0:
+        rand = Math.random()*(number.length);
+        password += number.charAt(rand).toString();
+        break;
+      case 1:
+        rand = Math.random()*(symbol.length);
+        password += symbol.charAt(rand).toString();
+        break;
+      case 2:
+        rand = Math.random()*(upper_cases.length);
+        password += upper_cases.charAt(rand).toString();
+        break;
+      case 3:
+        rand = Math.random()*(lower_cases.length);
+        password += lower_cases.charAt(rand).toString();
+        break;
+      case 4:
+        rand = Math.random()*(user_char.length);
+        password += user_char.charAt(rand).toString();
+        break;
+
+      default:
+          password = "Password DON'T Generated! Thank You!";
+    }
+  }
+  return password;
+}
+
+// set min and max password lenght
+function passwordLenght(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
 
 // Get references to the #generate element
